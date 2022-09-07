605cec89a2e5ff52b1dca6579ec49e3095acd019
keenPioneer <keenpioneer@gmail.com>
2022-09-07 12:15:33 +0530

    JFrame created

--- grid.java
+++ grid.java
@@ -1,9 +1,38 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class grid extends JFrame {
+public class grid extends JFrame implements MouseListener {
     grid(){
         this.setVisible(true);
         this.setSize(1000,1000);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addMouseListener(this);
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        System.out.println(mouseEvent);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
 
     }
 }
